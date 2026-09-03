c05df7db3b14c97663c95cc60a1838e15fdb25e1
Rushikesh Sanjay Kharche <rkrushikesh98@gmail.com>
2026-09-03 13:06:59 +0530

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
   allowedDevOrigins: ["192.168.31.85"],
 };
 
